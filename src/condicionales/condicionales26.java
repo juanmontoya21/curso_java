dec5612c376cc8439016b80b9c75d3117df29799
Practicante Manufactura  Juan Sebastian Montoya Vasquez <p.manufactura06@andercol.com.co>
2023-05-23 11:46:09 -0500

    se termina el ejercicio 26 condicionales

--- src/condicionales/condicionales26.java
+++ src/condicionales/condicionales26.java
@@ -16,39 +16,23 @@ public class condicionales26 {
         System.out.print("ingrese el tecer numero: ");
         numero3 = teclado.nextInt();
 
-
-
-
-        if(numero1 > numero2 && numero2 > numero3){
+        if(numero1 > numero2 && numero1 > numero3 && numero2 > numero3){
             System.out.println(numero1+">"+numero2+">"+numero3);
-        }else if(numero1 > numero2 && numero2 < numero3){
+        }else if(numero1 > numero2 && numero1 > numero3 && numero3 > numero2){
             System.out.println(numero1+">"+numero3+">"+numero2);
-        }else if(numero2 > numero1 && numero1 > numero3){
+        }else if(numero2 > numero1 && numero2 > numero3 && numero1 > numero3){
             System.out.println(numero2+">"+numero1+">"+numero3);
+        }else if(numero2 > numero1 && numero2 > numero3 && numero3 > numero1){
+            System.out.println(numero2+">"+numero3+">"+numero1);
+        }else if(numero3 > numero1 && numero3 > numero2 && numero1 > numero2){
+            System.out.println(numero3+">"+numero1+">"+numero2);
+        }else if(numero3 > numero1 && numero3 > numero2 && numero2 > numero1){
+            System.out.println(numero3+">"+numero2+">"+numero1);
+        }else {
+            System.out.println("todos son iguales");
         }
-        //else if(numero2 > numero1 && numero3 > numero1){
-            //System.out.println(numero2+">"+numero3+">"+numero1);
-        //}
 
-        // if(numero1 < numero2 && numero1 < numero3){
-        //     System.out.println(numero3+">"+numero2+">"+numero1);
-        // }else if (numero1 > numero2 && numero2 > numero3) {
-        //     System.out.println(numero1 + ">" + numero2 + ">" + numero3);
-        // }else if(numero1 > numero2 && numero2 < numero3){
-        //     System.out.println(numero1 + ">" + numero3 + ">" + numero2);
-        // }else if(numero2 > numero1 && numero3 > numero1){
-        //     System.out.println(numero2 + ">" + numero3 +">" +numero1);
-        // }else if(numero2 > numero1 && numero3 < numero1){
-        //     System.out.println(numero2 +">"+numero1+">"+numero3);
-        // }else if(numero3 > numero2 && numero2 > numero1){
-        //     System.out.println(numero3+">"+numero2+">"+numero1);
-        // }else if(numero3 > numero2 && numero2 < numero1){
-        //     System.out.println(numero3+">"+numero1+">"+numero2);
-        // }else if(numero1 < numero2 && numero1 < numero3){
-        //     System.out.println(numero3 + ">" + numero2 + ">" + numero1);
-        // }else{
-        //     System.out.println("todos iguales");
-        // }
+
 
     }
 }
